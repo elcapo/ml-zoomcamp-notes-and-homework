f37dfbb050b669928d65305d63bd2cf726cf1df9
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-10-05 10:48:57 +0100

    add notes for data analysis and setting up validation

--- module-2/notes.py
+++ module-2/notes.py
@@ -8,7 +8,10 @@ app = marimo.App(width="medium")
 def _():
     import marimo as mo
     import pandas as pd
-    return mo, pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    return mo, np, pd, sns
 
 
 @app.cell(hide_code=True)
@@ -214,9 +217,195 @@ def _(car_prices):
 
 @app.cell(hide_code=True)
 def _(car_prices, string_columns):
-    for column in string_columns:
-        car_prices[column] = car_prices[column].str.lower().str.replace(' ', '_')
+    for string_column in string_columns:
+        car_prices[string_column] = car_prices[string_column].str.lower().str.replace(' ', '_')
     car_prices.head()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    ## Exploratory Data Analysis
+
+    ### Check Unique Values per Column
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(car_prices):
+    unique_values = {}
+
+    for described_column in car_prices.columns:
+        unique_values[described_column] = {
+            "sample": car_prices[described_column].unique()[:5],
+            "count": car_prices[described_column].nunique()
+        }
+
+    unique_values
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    ### Check the Price Column
+
+    A tiny number of cars with a very high price is ruining our plot.
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(car_prices, sns):
+    sns.histplot(car_prices.msrp, bins=100)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""We could filter them to get a better view but that implies making a choice on where to cut. Which could confuse our model.""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(car_prices, sns):
+    sns.histplot(car_prices.msrp[car_prices.msrp < 100000], bins=100)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""Instead of making a manual cut, we can take the logarithm of the price. By doing so, outlier (few cases with extreme values) are handled in a more elegant way.""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(car_prices, np, sns):
+    price_logs = np.log1p(car_prices.msrp)
+
+    sns.histplot(price_logs, bins=100)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### Check for Empty Values""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(car_prices):
+    car_prices.isna().sum()[car_prices.isna().any()]
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    ## Setting Up the Validation Framework
+
+    We want to define our feature matrix $X$ and a target variable $y$ and then separate it into our train $X_T$ and $y_T$, validation $X_V$ and $y_V$ and test $X_{test}$ and $t_{test}$ splits.
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(car_prices):
+    n = len(car_prices)
+    n_val = n_test = int(len(car_prices) * 0.2)
+    n_train = len(car_prices) - n_val - n_test
+
+    {"n": n, "n_train": n_train, "n_val": n_val, "n_test": n_test}
+    return n, n_train, n_val
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    To avoid spliting the records in the same order they have in the dataset (so that we don't get oddly distributed splits), we'll make a randomized selection.
+
+    First, we create a list with the identifiers of each sample in a random order.
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(n, np):
+    np.random.seed(2)
+    shuffled_indexes = np.arange(n)
+    np.random.shuffle(shuffled_indexes)
+
+    shuffled_indexes
+    return (shuffled_indexes,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""Then we use that as our reference for creating our splits.""")
+    return
+
+
+@app.cell
+def _(car_prices, n_train, n_val, shuffled_indexes):
+    car_prices_train = car_prices.iloc[shuffled_indexes[:n_train]]
+    car_prices_val = car_prices.iloc[shuffled_indexes[n_train:n_train+n_val]]
+    car_prices_test = car_prices.iloc[shuffled_indexes[n_train+n_val:]]
+    return car_prices_test, car_prices_train, car_prices_val
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""As our index column has now a random order, we'll reset it in each of the splits.""")
+    return
+
+
+@app.cell
+def _(car_prices_test, car_prices_train, car_prices_val):
+    car_prices_train.reset_index(drop=True, inplace=True)
+    car_prices_val.reset_index(drop=True, inplace=True)
+    car_prices_test.reset_index(drop=True, inplace=True)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""Now, we'll define our target variables adjusting the price column using the logarithm function described above.""")
+    return
+
+
+@app.cell
+def _(car_prices_test, car_prices_train, car_prices_val, np):
+    y_train = np.log1p(car_prices_train.msrp.values)
+    y_val = np.log1p(car_prices_val.msrp.values)
+    y_test = np.log1p(car_prices_test.msrp.values)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""And, finally, we'll define our feature matrices.""")
+    return
+
+
+@app.cell
+def _(car_prices_test, car_prices_train, car_prices_val):
+    X_train = car_prices_train
+    X_val = car_prices_val
+    X_test = car_prices_test
+
+    del X_train["msrp"]
+    del X_val["msrp"]
+    del X_test["msrp"]
     return
 
 
