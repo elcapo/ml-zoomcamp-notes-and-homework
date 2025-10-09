59ef457df7c5c2a90daf523eb4d5339451d4857f
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-10-09 17:08:44 +0100

    add notes for the first chapter of module 3

--- module-3/notes.py
+++ module-3/notes.py
@@ -118,5 +118,141 @@ def _(chapters):
     return
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    ## Churn Prediction Project
+
+    We'll be working with the [Telco Customer Churn](https://www.kaggle.com/datasets/blastchar/telco-customer-churn) dataset from Kaggle, with the goal of implementing a model that's able to predict if a customer is likely to "churn" (stop using the service).
+
+    ### The Dataset
+
+    The raw data contains 7043 rows (customers) and 21 columns (features).
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(pd):
+    pd.DataFrame([
+        {
+            "column": "customerID",
+            "description": "Customer ID"
+        },
+        {
+            "column": "gender",
+            "description": "Whether the customer is a male or a female"
+        },
+        {
+            "column": "SeniorCitizen",
+            "description": "Whether the customer is a senior citizen or not (1, 0)"
+        },
+        {
+            "column": "Partner",
+            "description": "Whether the customer has a partner or not (Yes, No)"
+        },
+        {
+            "column": "Dependents",
+            "description": "Whether the customer has dependents or not (Yes, No)"
+        },
+        {
+            "column": "tenure",
+            "description": "Number of months the customer has stayed with the company"
+        },
+        {
+            "column": "PhoneService",
+            "description": "Whether the customer has a phone service or not (Yes, No)"
+        },
+        {
+            "column": "MultipleLines",
+            "description": "Whether the customer has multiple lines or not (Yes, No, No phone service)"
+        },
+        {
+            "column": "InternetService",
+            "description": "Customer’s internet service provider (DSL, Fiber optic, No)"
+        },
+        {
+            "column": "OnlineSecurity",
+            "description": "Whether the customer has online security or not (Yes, No, No internet service)"
+        },
+        {
+            "column": "OnlineBackup",
+            "description": "Whether the customer has online backup or not (Yes, No, No internet service)"
+        },
+        {
+            "column": "DeviceProtection",
+            "description": "Whether the customer has device protection or not (Yes, No, No internet service)"
+        },
+        {
+            "column": "TechSupport",
+            "description": "Whether the customer has tech support or not (Yes, No, No internet service)"
+        },
+        {
+            "column": "StreamingTV",
+            "description": "Whether the customer has streaming TV or not (Yes, No, No internet service) "
+        },
+        {
+            "column": "StreamingMovies",
+            "description": "Whether the customer has streaming movies or not (Yes, No, No internet service)"
+        },
+        {
+            "column": "Contract",
+            "description": "The contract term of the customer (Month-to-month, One year, Two year)"
+        },
+        {
+            "column": "PaperlessBilling",
+            "description": "Whether the customer has paperless billing or not (Yes, No)"
+        },
+        {
+            "column": "PaymentMethod",
+            "description": "The customer’s payment method (Electronic check, Mailed check, Bank transfer (automatic), Credit card"
+        },
+        {
+            "column": "MonthlyCharges",
+            "description": "The amount charged to the customer monthly"
+        },
+        {
+            "column": "TotalCharges",
+            "description": "The total amount charged to the customer"
+        },
+        {
+            "column": "Churn",
+            "description": "Whether the customer churned or not (Yes or No)"
+        },
+    ])
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    /// admonition | Local Dataset Path
+
+    For convenience, the dataset has already been downloaded into [module-3/data/customer-churn.csv](module-3/data/customer-churn.csv).
+    ///
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    ### The Goal
+
+    What we'll try to do is to predict the **Churn** column, which will be our target variable and represents the customers who actually left the service.
+
+    ### The Method
+
+    We'll train a binary classification model that will attempt to classify customers in two groups, the ones that are right about to churn and the ones who are not.
+    """
+    )
+    return
+
+
 if __name__ == "__main__":
     app.run()
