8a2bda50b56926a5acda3df1768d5b77ab29dc54
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-12-15 11:56:23 +0000

    add a working request example

--- module-10/workshop/main.py
+++ module-10/workshop/main.py
@@ -1,16 +1,37 @@
+from typing import Annotated
 from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 import model
 
 class Request(BaseModel):
-    url: str = Field(..., example="https://raw.githubusercontent.com/alexeygrigorev/clothing-dataset-small/master/test/pants/4aabd82c-82e1-4181-a84d-d0c6e550d26d.jpg")
+    url: HttpUrl
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "url": "https://raw.githubusercontent.com/alexeygrigorev/clothing-dataset-small/master/test/pants/4aabd82c-82e1-4181-a84d-d0c6e550d26d.jpg"
+                }
+            ]
+        }
+    }
+
+class Response(BaseModel):
+    prediction: str
+    logits: dict[str, float]
 
 app = FastAPI(title="hair-classifier")
 
 @app.post("/predict")
-def predict(request: Request) -> dict:
-    return model.predict(request.url)
+def predict(request: Request) -> Response:
+    logits = model.predict(str(request.url))
+    prediction = max(logits, key=logits.get)
+
+    return Response(
+        prediction=prediction,
+        logits=logits,
+    )
 
 @app.get("/health")
 def health() -> dict:
